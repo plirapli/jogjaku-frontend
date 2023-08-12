7848fb2161a763184ef13df90bf9a28dcf8fbcd0
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-08-12 21:40:12 +0700

    navbar done babi

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -1,63 +1,35 @@
+import { Icon } from '@iconify/react';
+
 const NavbarMenu = () => {
+  const listItems = [
+    { title: 'Beranda', href: '/' },
+    { title: 'Destinasi', href: '/destinasi' },
+    { title: 'Event', href: '/event' },
+  ];
+
+  console.log(listItems.map((listItem) => listItem.title));
+
   return (
-    <nav className='bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600'>
+    <nav className='bg-yellow-50 fixed w-full z-20 top-0 left-0 border-b border-gray-200'>
       <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        {/* Logo */}
         <a href='https://flowbite.com/' className='flex items-center'>
           <img
-            src='https://flowbite.com/docs/images/logo.svg'
+            src='/src/assets/logo-primary.svg'
             className='h-8 mr-3'
             alt='Flowbite Logo'
           />
-          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
-            Flowbite
-          </span>
         </a>
 
-        {/* Menu */}
-        <div
-          className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
-          id='navbar-user'
-        >
-          <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-            <li>
-              <a
-                href='#'
-                className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
-                aria-current='page'
-              >
-                Beranda
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
-                Destinasi
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
-                Event
-              </a>
-            </li>
-            <li>
-              <a
-                href='#'
-                className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
-                Keranjang
-              </a>
-            </li>
-          </ul>
+        {/* Profil */}
+        <div className='flex items-center md:order-2'>
           <button
             type='button'
-            className='flex ml-5 mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600'
+            className='flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600'
             id='user-menu-button'
             aria-expanded='false'
+            data-dropdown-toggle='user-dropdown'
+            data-dropdown-placement='bottom'
           >
             <span className='sr-only'>Open user menu</span>
             <img
@@ -66,10 +38,103 @@ const NavbarMenu = () => {
               alt='user photo'
             />
           </button>
+          <div
+            className='z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600'
+            id='user-dropdown'
+          >
+            <div className='px-4 py-3'>
+              <span className='block text-sm text-gray-900 dark:text-white'>
+                Bonnie Green
+              </span>
+              <span className='block text-sm  text-gray-500 truncate dark:text-gray-400'>
+                name@flowbite.com
+              </span>
+            </div>
+            <ul className='py-2' aria-labelledby='user-menu-button'>
+              <li>
+                <a
+                  href='#'
+                  className='flex items-center gap-1.5 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'
+                >
+                  <Icon icon='iconamoon:profile' width='16' />
+                  Profil
+                </a>
+              </li>
+              <li>
+                <a
+                  href='#'
+                  className='flex items-center gap-1.5 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100'
+                >
+                  <Icon icon='mdi:cart-outline' width='16' />
+                  Keranjang
+                </a>
+              </li>
+              <div className='h-[1px] w-full px-2 bg-gray-200'></div>
+              <li>
+                <a
+                  href='#'
+                  className='flex items-center gap-1.5 px-4 py-2 text-sm text-red-500 hover:bg-gray-100'
+                >
+                  <Icon icon='mingcute:exit-line' width='16' />
+                  Keluar
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button
+            data-collapse-toggle='navbar-sticky'
+            type='button'
+            className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+            aria-controls='navbar-sticky'
+            aria-expanded='false'
+          >
+            <span className='sr-only'>Open main menu</span>
+            <svg
+              className='w-5 h-5'
+              aria-hidden='true'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 17 14'
+            >
+              <path
+                stroke='currentColor'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M1 1h15M1 7h15M1 13h15'
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Menu tengah */}
+        <div
+          className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
+          id='navbar-sticky'
+        >
+          <ul className='flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent'>
+            {listItems.map((listItem) => (
+              <ListItem
+                key={listItem.title}
+                href={listItem.href}
+                className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0'
+              >
+                {listItem.title}
+              </ListItem>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
   );
 };
+
+const ListItem = ({ href, className, children, ...props }) => (
+  <li>
+    <a href={href} className={className} {...props}>
+      {children}
+    </a>
+  </li>
+);
 
 export default NavbarMenu;
