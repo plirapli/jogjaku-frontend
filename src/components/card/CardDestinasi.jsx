0c157d0f5b0bb04aeb549c94b420a42eb12be63e
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-08-13 22:08:04 +0700

    card buat destinasi done

--- src/components/card/CardDestinasi.jsx
+++ src/components/card/CardDestinasi.jsx
@@ -1,32 +1,33 @@
 import { Link } from 'react-router-dom';
-import defaultCover from '../../assets/default_cover.jpg';
+import defaultCover from '../../assets/default-destination.jpg';
 
 const CardWork = ({ work }) => {
   const imgCover = work?.img_cover || defaultCover;
 
   return (
-    <div className='flex flex-col gap-2.5 border bg-white rounded-md shadow overflow-hidden'>
-      <div className='bg-gray-400 border-b h-36 w-full'>
-        <img src={imgCover} alt='' className='object-cover h-full w-full' />
-      </div>
-      <div className='flex-1 flex flex-col p-4 pt-0'>
-        <div className='text-sm text-primary capitalize font-medium clamp'>
-          Lorem Ipsum
-        </div>
-        <div className='flex-1 mt-0.5 text-sm clamp'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          dolores, ad blanditiis ducimus at distinctio deserunt magni itaque
-          possimus provident? Excepturi iusto consectetur inventore, aperiam
-          saepe amet nam possimus commodi.
-        </div>
-        <Link
+    <Link>
+      <div className='flex flex-col justify-end border rounded-md shadow overflow-hidden relative h-80 cursor-pointer transition-all hover:shadow-lg hover:-translate-y-1'>
+        <img
+          src={imgCover}
+          alt=''
+          className='-z-10 absolute object-cover h-full w-full'
+        />
+        <div className='flex flex-col p-4 backdrop-blur-sm bg-black/40 text-white'>
+          <div className='text-xl text-primary capitalize font-medium clamp'>
+            Candi Borobudur
+          </div>
+          <div className='flex-1 text-white text-opacity-75'>
+            Borobudur Temple
+          </div>
+          {/* <Link
           to={`/work/`}
           className='link mt-2.5 text-sm text-black text-opacity-50'
         >
           See details
-        </Link>
+        </Link> */}
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
