af485a4788771e22e5e983e32aaaead07091d9f2
Rafli <rafliyulistiawan@gmail.com>
2023-08-23 15:54:47 +0700

    delete forgot pasword

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -2,6 +2,5 @@ import { lazy } from 'react';
 
 const Login = lazy(() => import('./Login'))
 const Register = lazy(() => import('./Register'))
-const ForgotPassword = lazy(() => import('./ForgotPassword'))
 
-export { Login, Register, ForgotPassword };
+export { Login, Register };
