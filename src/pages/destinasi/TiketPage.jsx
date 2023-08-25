342b5942058e7f4a6e98e045fb26fe51c4ae190d
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-08-25 22:46:03 +0700

    error handling for ticketless destination

--- src/pages/destinasi/TiketPage.jsx
+++ src/pages/destinasi/TiketPage.jsx
@@ -8,28 +8,33 @@ const TiketPage = () => {
 
   return (
     <div>
-      <div className='flex flex-col gap-2 sm:flex-row sm:gap-4 items-center'>
-        <div className='w-full'>
-          <h2 className='text-lg font-medium'>Beli Tiket</h2>
-          <p className='text-black text-opacity-50'>{destination?.name}</p>
+      {destination?.destinationTickets.length ? (
+        <div className='flex flex-col gap-2 sm:flex-row sm:gap-4 items-center'>
+          <div className='w-full'>
+            <h2 className='text-lg font-medium'>Beli Tiket</h2>
+            <p className='text-black text-opacity-50'>{destination?.name}</p>
+          </div>
+          <div className='w-full flex items-center gap-4'>
+            <label
+              htmlFor='first_name'
+              className='min-w-fit text-sm font-medium text-gray-900 dark:text-white'
+            >
+              Pilih Tanggal
+            </label>
+            <input
+              type='date'
+              onChange={(e) => setDate(e.target.value)}
+              value={date}
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
+              placeholder='Select date'
+            />
+          </div>
         </div>
-        <div className='w-full flex items-center gap-4'>
-          <label
-            htmlFor='first_name'
-            className='min-w-fit text-sm font-medium text-gray-900 dark:text-white'
-          >
-            Pilih Tanggal
-          </label>
-          <input
-            type='date'
-            onChange={(e) => setDate(e.target.value)}
-            value={date}
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 '
-            placeholder='Select date'
-          />
-        </div>
-      </div>
+      ) : (
+        <div className='font-medium text-center'>Tiket tidak ada.</div>
+      )}
       <div className='divider'></div>
+
       <div className='space-y-3'>
         {destination?.destinationTickets?.map((ticket) => (
           <CardBeliTiket
