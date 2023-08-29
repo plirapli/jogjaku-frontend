d8a2df2620a23560ef0b7d0cb0de8f60a4eaa786
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-08-30 00:35:17 +0700

    add loading screen, empty state

--- src/pages/tiket-saya/TiketSayaPendingPage.jsx
+++ src/pages/tiket-saya/TiketSayaPendingPage.jsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from 'react';
 import { CardTransaksi } from '../../components/card';
 import { getOrderPending } from '../../utils/order';
+import Loading from '../../components/loading/Loading';
 
 const TiketSayaPendingPage = () => {
-  const [allPendingPayment, setAllPendingPayment] = useState([]);
+  const [allPayment, setAllPayment] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getOrderPending()
-      .then(setAllPendingPayment)
+      .then(setAllPayment)
       .catch((err) => {
         console.error(err);
-      });
+      })
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <div className='mt-2 space-y-3'>
-      {allPendingPayment.map((payment) => (
-        <CardTransaksi key={payment?.id} payment={payment} />
-      ))}
+      {!isLoading ? (
+        allPayment?.length > 0 ? (
+          allPayment?.map((payment) => (
+            <CardTransaksi key={payment?.id} payment={payment} />
+          ))
+        ) : (
+          <div className='text-gray-dark'>
+            Kamu tidak memiliki pembayaran pending.
+          </div>
+        )
+      ) : (
+        <div className='mt-2 w-full flex justify-center'>
+          <Loading />
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/tiket-saya/TiketSayaSemuaPage.jsx
+++ src/pages/tiket-saya/TiketSayaSemuaPage.jsx
@@ -1,23 +1,38 @@
 import { useEffect, useState } from 'react';
 import { getOrderHistory } from '../../utils/order';
 import { CardTransaksi } from '../../components/card/';
+import Loading from '../../components/loading/Loading';
 
 const TiketSayaSemuaPage = () => {
   const [allPayment, setAllPayment] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getOrderHistory()
       .then(setAllPayment)
       .catch((err) => {
         console.error(err);
-      });
+      })
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <div className='mt-2 space-y-3'>
-      {allPayment.map((payment) => (
-        <CardTransaksi key={payment?.id} payment={payment} />
-      ))}
+      {!isLoading ? (
+        allPayment?.length > 0 ? (
+          allPayment?.map((payment) => (
+            <CardTransaksi key={payment?.id} payment={payment} />
+          ))
+        ) : (
+          <div className='text-gray-dark'>
+            Kamu belum pernah melakukan transaksi.
+          </div>
+        )
+      ) : (
+        <div className='mt-2 w-full flex justify-center'>
+          <Loading />
+        </div>
+      )}
     </div>
   );
 };
