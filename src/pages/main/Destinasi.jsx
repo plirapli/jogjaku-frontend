0764d0772de3e446f0c3d518177b322a2f5e38ef
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-09-25 12:03:03 +0700

    fix destination per region bug

--- src/pages/main/Destinasi.jsx
+++ src/pages/main/Destinasi.jsx
@@ -1,17 +1,26 @@
 import ConstraintLarge from '../../layout/ConstraintLarge';
 import CardDestinasi from '../../components/card/CardDestinasi';
 import { Fragment, useEffect, useState } from 'react';
-import { getAllDestinations } from '../../utils/destination';
+import {
+  getAllDestinations,
+  getAllDestinationsPerRegion,
+} from '../../utils/destination';
 import Loading from '../../components/loading/Loading';
 import { Tab } from '@headlessui/react';
 
 const Destinasi = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [destinations, setDestinations] = useState([]);
+  const [destinations, setDestinations] = useState({});
+  const [selectedRegion, setSelectedRegion] = useState(0);
+
+  const changeRegionHandler = (region) => {
+    console.log(region);
+    setSelectedRegion(region);
+  };
 
   useEffect(() => {
-    getAllDestinations()
-      .then(({ destinations }) => setDestinations(() => [...destinations]))
+    getAllDestinationsPerRegion()
+      .then(setDestinations)
       .catch(({ data }) => {
         console.log(data);
       })
@@ -40,12 +49,16 @@ const Destinasi = () => {
             <Loading />
           </div>
         ) : (
-          <Tab.Group>
+          <Tab.Group
+            selectedIndex={selectedRegion}
+            onChange={setSelectedRegion}
+          >
             <Tab.List className='flex flex-col rounded-3xl sm:flex-row sm:space-x-1 sm:rounded-full bg-[#EEEEEE] border p-1'>
               {menuDaerah.map((daerah, i) => (
                 <Tab key={i} as={Fragment}>
                   {({ selected }) => (
                     <button
+                      // onClick={() => changeRegionHandler}
                       className={`w-full rounded-full py-2.5 text-sm leading-5 ring-white focus:outline-none focus:ring-2 ${
                         selected
                           ? 'shadow bg-primary text-white ring-offset-white'
@@ -58,10 +71,12 @@ const Destinasi = () => {
                 </Tab>
               ))}
             </Tab.List>
-            <Tab.Panels className='mt-4 layout'>
-              {destinations.map((destination) => (
-                <Tab.Panel key={destination?.id}>
-                  <CardDestinasi data={destination} />
+            <Tab.Panels className='mt-4'>
+              {Object.values(destinations).map((regencies, i) => (
+                <Tab.Panel key={i} className='layout'>
+                  {regencies.map((destination) => (
+                    <CardDestinasi key={destination?.id} data={destination} />
+                  ))}
                 </Tab.Panel>
               ))}
             </Tab.Panels>
--- src/utils/destination.js
+++ src/utils/destination.js
@@ -7,6 +7,29 @@ const getAllDestinations = async () =>
     .then((data) => data.data)
     .catch(({ response }) => Promise.reject(response));
 
+const getAllDestinationsPerRegion = async () => {
+  return api
+    .get('/destinations')
+    .then((data) => {
+      const { destinations } = data.data
+      const sleman = destinations.filter((destination) => destination.regency === 'Sleman')
+      const yogyakarta = destinations.filter((destination) => destination.regency === 'Kota Yogyakarta')
+      const gk = destinations.filter((destination) => destination.regency === 'Gunung Kidul')
+      const kp = destinations.filter((destination) => destination.regency === 'Kulon Progo')
+      const bantul = destinations.filter((destination) => destination.regency === 'Bantul')
+      const allDestinations = {
+        semua: [...destinations],
+        sleman: [...sleman],
+        yogyakarta: [...yogyakarta],
+        gk: [...gk],
+        kp: [...kp],
+        bantul: [...bantul],
+      }
+      return allDestinations
+    })
+    .catch(({ response }) => Promise.reject(response));
+}
+
 // GET Destination by ID
 const getDestinationByID = async (id) =>
   api
@@ -20,4 +43,4 @@ const getDestinationTicket = async (id) =>
     .then(({ data }) => data.destination.destinationTickets)
     .catch(({ response }) => Promise.reject(response));
 
-export { getAllDestinations, getDestinationByID, getDestinationTicket }
+export { getAllDestinations, getAllDestinationsPerRegion, getDestinationByID, getDestinationTicket }
