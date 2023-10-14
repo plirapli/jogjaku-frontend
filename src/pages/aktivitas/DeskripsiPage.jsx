8727aa19e695110cca31bf34c7851098f1b6520a
rafliyulistiawan <rafliyulistiawan@gmail.com>
2023-10-14 22:21:08 +0700

    add read more button

--- src/pages/aktivitas/DeskripsiPage.jsx
+++ src/pages/aktivitas/DeskripsiPage.jsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
 
 const DeskripsiPage = () => {
   const aktivitas = useOutletContext();
+  const [isReadMore, setIsReadMore] = useState(false);
+  const onChangeReadMoreHandle = () => {
+    setIsReadMore((prev) => !prev);
+  };
 
   return (
     <div>
       <div>
-        <h2 className='font-bold text-primaryDark'>Deskripsi Umum</h2>
-        <div className='mt-1 flex flex-col gap-2'>
-          <p>{aktivitas?.description}</p>
+        <h2 className='font-medium text-primaryDark'>Deskripsi Umum</h2>
+        <div className=' mt-1 flex flex-col gap-2'>
+          <p
+            className={`text-black text-opacity-75 ${
+              !isReadMore && 'line-clamp-4'
+            }`}
+          >
+            {aktivitas?.description}
+          </p>
+          <button
+            onClick={onChangeReadMoreHandle}
+            className='mt-0.5 w-fit px-4 py-2 border text-black text-opacity-60 text-sm rounded-md
+            transition-all hover:text-opacity-100'
+          >
+            {isReadMore ? 'Tampilkan lebih sedikit' : 'Baca selengkapnya'}
+          </button>
         </div>
       </div>
       <div className='mt-4'>
-        <h2 className='font-bold text-primaryDark'>Lokasi</h2>
+        <h2 className='font-medium text-primaryDark'>Lokasi</h2>
         <div className='mt-1 rounded-xl overflow-hidden border-2 border-primary'>
           <iframe
             className='w-full'
--- src/pages/event/DeskripsiPage.jsx
+++ src/pages/event/DeskripsiPage.jsx
@@ -1,30 +1,48 @@
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
 
 const DeskripsiPage = () => {
   const event = useOutletContext();
+  const [isReadMore, setIsReadMore] = useState(false);
+  const onChangeReadMoreHandle = () => {
+    setIsReadMore((prev) => !prev);
+  };
 
   return (
     <div>
-      <div>
-        <h2 className='font-bold text-primaryDark'>Deskripsi Umum</h2>
+      <div className=''>
+        <h2 className='font-medium text-primaryDark'>Tanggal</h2>
+        <div className='mt-2 flex items-center gap-2 p-3 border rounded-lg'>
+          <Icon
+            className='text-black text-opacity-50'
+            icon='ic:twotone-date-range'
+            width='22'
+          />
+          <p className='text-black text-opacity-75'>{event?.date}</p>
+        </div>
+      </div>
+      <div className='mt-4'>
+        <h2 className='font-medium text-primaryDark'>Deskripsi Umum</h2>
         <div className=' mt-1 flex flex-col gap-2'>
-          <p>{event?.description}</p>
+          <p
+            className={`text-black text-opacity-75 ${
+              !isReadMore && 'line-clamp-4'
+            }`}
+          >
+            {event?.description}
+          </p>
+          <button
+            onClick={onChangeReadMoreHandle}
+            className='mt-0.5 w-fit px-4 py-2 border text-black text-opacity-60 text-sm rounded-md
+            transition-all hover:text-opacity-100'
+          >
+            {isReadMore ? 'Tampilkan lebih sedikit' : 'Baca selengkapnya'}
+          </button>
         </div>
       </div>
       <div className='mt-4'>
-        <h2 className='font-bold text-primaryDark'>Tanggal</h2>
-        <div className='mt-1 flex flex-col gap-2'>
-          <p>{event?.date}</p>
-        </div>
-      </div>
-      <div className='mt-4'>
-        <h2 className='font-bold text-primaryDark'>Penyelenggara</h2>
-        <div className='mt-1 flex flex-col gap-2'>
-          <p>{event?.organizer}</p>
-        </div>
-      </div>
-      <div className='mt-4'>
-        <h2 className='font-bold text-primaryDark'>Lokasi</h2>
+        <h2 className='font-medium text-primaryDark'>Lokasi</h2>
         <div className='mt-1 rounded-xl overflow-hidden border-2 border-primary'>
           <iframe
             className='w-full'
